bd83cd5cfaa647162a0ed9df67e1f8b5ca884b70
Mike Shenin <i@kotrik.ru>
2018-12-04 22:37:19 +0300

    ChatMeta slow give fix + add function like calling all before,after_events without prefix and command

--- Yui.py
+++ Yui.py
@@ -14,11 +14,12 @@ from common.VK.VKWrappers import make_upload_docs, make_upload_photo, make_reply
 
 import common.VK.Chatmeta as ChatMeta
 
+
 class Yui:
 
     def __init__(self):
         self.store = store
-        self.ver = "0.2.0 beta"
+        self.ver = "0.2.566 beta"
         self.store.config.ver = self.ver
 
         Logger.Ylog(f"> Привет, я Yui! Бот для социальной сети ВК.\n> На данный момент моя версия: {self.ver}")
@@ -58,6 +59,35 @@ class Yui:
     def say_goodbye(self):
         Logger.Ylog("Пока семпай, надеюсь ещё встретимся!")
 
+    async def call_events(self, updated_message, args, prefix, command, what_events_need=None):
+        if what_events_need is None:
+            what_events_need = [1, 2, 3]
+
+        currentStore = ObjDict()
+        currentStore.reply = make_reply(self.store, updated_message.peer_id, currentStore)
+        currentStore.upload_photo = make_upload_photo(self.store, updated_message.peer_id)
+        currentStore.upload_doc = make_upload_docs(self.store, updated_message.peer_id)
+        currentStore.request = self.store.wrappers.request
+        if command:
+            currentStore.cmd = command
+            currentStore.args = args
+            currentStore.prefix = prefix
+
+        if self.store.config.UseChatMeta:
+            await ChatMeta.call_chat_meta(currentStore, updated_message)
+
+        if 1 in what_events_need:
+            if await self.store.call_before_events(updated_message, currentStore):
+                return True
+
+        if 2 in what_events_need:
+            if command:
+                await self.store.call_command(updated_message, currentStore)
+
+        if 3 in what_events_need:
+            if await self.store.call_after_events(updated_message, currentStore):
+                return True
+
     async def process_update(self, update):
         updated_message = await Converter.convert_to_message(update)
 
@@ -70,7 +100,7 @@ class Yui:
                 prefix = lprefix
                 break
         if not prefix:
-            return True
+            return await self.call_events(updated_message, None, None, None, what_events_need=[1, 3])
 
         command = None
         for (k, v) in store.handlers.items():
@@ -98,24 +128,4 @@ class Yui:
             self.store.cd_users[updated_message.from_id] = {}
             self.store.cd_users[updated_message.from_id]['message_date'] = ts
 
-        currentStore = ObjDict()
-        currentStore.reply = make_reply(self.store, updated_message.peer_id, currentStore)
-        currentStore.upload_photo = make_upload_photo(self.store, updated_message.peer_id)
-        currentStore.upload_doc = make_upload_docs(self.store, updated_message.peer_id)
-        currentStore.request = self.store.wrappers.request
-        if command:
-            currentStore.cmd = command
-            currentStore.args = args
-            currentStore.prefix = prefix
-
-        if self.store.config.UseChatMeta:
-            await ChatMeta.call_chat_meta(currentStore, updated_message)
-
-        if await self.store.call_before_events(updated_message, currentStore):
-            return True
-
-        if command:
-            await self.store.call_command(updated_message, currentStore)
-
-        if await self.store.call_after_events(updated_message, currentStore):
-            return True
+        return await self.call_events(updated_message, args, prefix, command)
--- common/VK/Chatmeta.py
+++ common/VK/Chatmeta.py
@@ -1,4 +1,3 @@
-from utils import check_admin
 from common.Store import store as gstore
 
 class ChatData:
@@ -13,6 +12,7 @@ class ChatData:
         self.previous_groups = []
 
 chats = {}
+cached_admins = {}
 
 async def get_chat_data(update, store, peer_id, refresh=False):
     if not refresh and peer_id in chats:
@@ -36,6 +36,7 @@ async def get_chat_data(update, store, peer_id, refresh=False):
         chat_data.previous_users = chats[peer_id].users
         chat_data.previous_groups = chats[peer_id].groups
 
+    chats[peer_id] = chat_data
     return chat_data
 
 def create_refresh(message, store, peer_id):
@@ -45,13 +46,39 @@ def create_refresh(message, store, peer_id):
     return func
 
 
+async def check_admin(store, update, peer_id, uid):
+    req = await store.request('messages.getConversationMembers', peer_id=update.peer_id, fields="sex,screen_name,nickname,invited_by")
+    if not 'items' in req.response:
+        cached_admins[peer_id] = False
+        return False
+
+    for x in req.response['items']:
+        if x['member_id'] == uid*-1:
+            if 'is_admin' in x and x['is_admin']:
+                cached_admins[peer_id] = True
+                return True
+            cached_admins[peer_id] = False
+            return False
+
+        if x['member_id'] == uid:
+            if 'is_admin' in x and x['is_admin']:
+                cached_admins[peer_id] = True
+                return True
+            cached_admins[peer_id] = False
+            return False
+
+    cached_admins[peer_id] = False
+    return False
+
 async def call_chat_meta(store, update):
     if not update.is_multichat:
         store.meta_data = None
         store.meta_refresh = None
         return None
 
-    if not await check_admin(store, update, update.peer_id, gstore.config.group_id):
+    if update.peer_id in cached_admins and cached_admins.get(update.peer_id):
+        pass
+    elif not await check_admin(store, update, update.peer_id, gstore.config.group_id):
         store.meta_data = None
         store.meta_refresh = None
         return None
@@ -60,6 +87,12 @@ async def call_chat_meta(store, update):
     store.meta_refresh = create_refresh(update, store, update.peer_id)
     if update.raw_update['object'].get('action', 0) != 0:
         if update.raw_update['object']['action']['type'] == 'chat_invite_user' or update.raw_update['object']['action']['type'] == "chat_kick_user":
+            if update.raw_update['object']['action']['type'] == 'chat_kick_user':
+                if update.peer_id in cached_admins:
+                    del(cached_admins[update.peer_id])
+                if update.peer_id in chats:
+                    del(chats[update.peer_id])
+
             await store.meta_refresh()
 
     return True
